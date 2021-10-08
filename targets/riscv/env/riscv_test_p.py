8f1fe5f2ed5f6b1d45bbd0a66b9abc021ccd4763
Ramon Bertran Monfort <rbertra@us.ibm.com>
2021-10-08 18:17:15 -0400

    [FIX] Implement RISCV volatile_registers
    Signed-off-by: Ramon Bertran Monfort <rbertra@us.ibm.com>

--- targets/riscv/env/riscv_test_p.py
+++ targets/riscv/env/riscv_test_p.py
@@ -90,3 +90,19 @@ class riscv64_test_p(GenericEnvironment):
                               return_address_reg,
                               self.target.isa.registers["X0"]])
         return [ret_ins]
+
+    @property
+    def volatile_registers(self):
+
+        rlist = []
+        for idx in [
+                1, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17,
+                28, 29, 30, 31]:
+            rlist += [self.target.registers['X%d' % idx]]
+
+        for idx in [
+                0, 1, 2, 3, 4, 5, 6, 7, 10, 11, 12, 13, 14, 15, 16, 17,
+                28, 29, 30, 31]:
+            rlist += [self.target.registers['F%d' % idx]]
+
+        return rlist
