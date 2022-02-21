1396cfc79359370341b36b79805e87e7b9400e89
Ramon Bertran Monfort <rbertra@us.ibm.com>
2022-02-21 14:09:45 -0500

    Revert mp_mpt2test.py changes
    Signed-off-by: Ramon Bertran Monfort <rbertra@us.ibm.com>

--- targets/generic/tools/mp_mpt2test.py
+++ targets/generic/tools/mp_mpt2test.py
@@ -27,9 +27,6 @@ import sys
 # Own modules
 import microprobe
 import microprobe.code
-from microprobe.code.address import InstructionAddress
-from microprobe.code.context import Context
-from microprobe.code.ins import instruction_to_definition
 import microprobe.passes.address
 import microprobe.passes.branch
 import microprobe.passes.ilp
@@ -55,8 +52,7 @@ LOG = get_logger(__name__)
 
 
 # Functions
-def generate(test_definition, outputfile, ldscriptfile, target,
-             **kwargs):
+def generate(test_definition, outputfile, target, **kwargs):
     """
     Microbenchmark generation policy
 
@@ -147,11 +143,7 @@ def generate(test_definition, outputfile, ldscriptfile, target,
                 raw_dict[address], target, safe=True, little_endian=False,
                 word_length=4
             )
-            code[0].address = address + test_definition.default_code_address
-            for i in range(1, len(code)):
-                assert code[i-1].get_instruction_type().format.length > 0
-                code[i].address = code[i-1].address + \
-                    code[i-1].get_instruction_type().format.length
+            code[0].address = address
             sequence.extend(code)
 
     else:
@@ -169,116 +161,22 @@ def generate(test_definition, outputfile, ldscriptfile, target,
     registers = test_definition.registers
     raw = test_definition.raw
 
+    for instruction_def in sequence:
+        if instruction_def.address is not None:
+            print_warning(
+                "Instruction address not needed in '%s'" % instruction_def.asm
+            )
+
     if test_definition.default_data_address is not None:
         print_warning("Default data address not needed")
 
-    wrapper_kwargs = {}
+    if test_definition.default_code_address is not None:
+        print_warning("Default code address not needed")
 
+    wrapper_name = "CWrapper"
     if kwargs.get("endless", False):
-        wrapper_name = target.environment.default_endless_wrapper
-    else:
-        wrapper_name = target.environment.default_wrapper
-
-    if wrapper_name == "AssemblyLd":
-        if sequence[0].label is not None:
-            start_label = sequence[0].label
-        else:
-            start_label = sequence[0].label = "START_TEST"
-
-        code_sections = []
-        for addr in raw_dict.keys():
-            code_sections.append(addr + test_definition.default_code_address)
-
-        wrapper_kwargs["sections"] = code_sections
-        wrapper_kwargs["start_label"] = start_label
-
-        start_test_instructions = []
-
-        if kwargs.get("wrap_function", False):
-            print("Adding function wrapping instructions...")
-            target_address = "0x%x" % test_definition.default_code_address
-            instructions = target.function_call(target_address)
-            instructions[0].set_label("LOOP_START")
-
-            for instr in instructions:
-                instr_def = instruction_to_definition(instr)
-                start_test_instructions.append(instr_def)
-        else:
-            print("Adding jump to test instructions...")
-            source_address = InstructionAddress(
-                base_address="dummy",  # Rely on symbolic jump
-                displacement=0
-            )
-
-            target_address = InstructionAddress(
-                base_address=start_label,
-                displacement=0
-            )
-
-            instr = target.branch_unconditional_relative(
-                    source_address,
-                    target_address
-            )
-
-            instr.set_label("LOOP_START")
-
-            start_test_instructions.append(instruction_to_definition(instr))
-
-        if kwargs.get("endless", False):
-            context = Context()  # Throw-away context
-            reset_instructions = []
-
-            if kwargs.get("reset_registers", False):
-                # TODO: The following register lists are hard-coded for RISCV,
-                # there needs to be an API for each target/env to define its
-                # own set of registers
-                all_registers = ["X%d" % n for n in range(1, 32)]
-                # This contains the registers involved in argument passing
-                # when calling functions (as per the ABI).
-                arg_abi_registers = ["X%d" % n for n in range(10, 18)]
-
-                if kwargs.get("wrap_function", None):
-                    reset_registers = arg_abi_registers
-                else:
-                    reset_registers = all_registers
-
-                register_dict = {reg.name: reg.value for reg in registers}
-                for register in target.registers.values():
-                    if (register.name in reset_registers and
-                            register.name in register_dict):
-                        value = register_dict[register.name]
-                        instrs = target.set_register(register, value, context)
-                        for instr in instrs:
-                            reset_instructions.append(instr)
-
-            source_address = InstructionAddress(
-                base_address="dummy",  # Rely on symbolic jump
-                displacement=0
-            )
-
-            target_address = InstructionAddress(
-                base_address="LOOP_START",
-                displacement=0
-            )
-
-            branch_instruction = target.branch_unconditional_relative(
-                    source_address,
-                    target_address
-            )
-
-            reset_instructions.append(branch_instruction)
-
-            old_instructions = reset_instructions
-            reset_instructions = []
-            for instr in old_instructions:
-                reset_instructions.append(instruction_to_definition(instr))
-
-            if kwargs.get("wrap_function", False):
-                start_test_instructions.extend(reset_instructions)
-            else:
-                sequence.extend(reset_instructions)
-
-        sequence = start_test_instructions + sequence
+        print_warning("Using endless C wrapper")
+        wrapper_name = "CInfGen"
 
     print_info("Creating benchmark synthesizer...")
 
@@ -288,10 +186,11 @@ def generate(test_definition, outputfile, ldscriptfile, target,
         raise MicroprobeException(
             "Wrapper '%s' not available. Check if you have the wrappers "
             "of the target installed or set up an appropriate "
-            "MICROPROBEWRAPPERS environment variable. "
-            "Original error was: %s" % (wrapper_name, str(exc))
+            "MICROPROBEWRAPPERS environment variable. Original error was: %s" %
+            (wrapper_name, str(exc))
         )
 
+    wrapper_kwargs = {}
     wrapper = cwrapper(**wrapper_kwargs)
 
     synth = microprobe.code.Synthesizer(target, wrapper, extra_raw=raw)
@@ -344,35 +243,6 @@ def generate(test_definition, outputfile, ldscriptfile, target,
 
     # Save the microbenchmark
     synth.save(outputfile, bench=bench)
-
-    if wrapper_name == "AssemblyLd":
-        print_info("Generating linker script...")
-        script_contents = "SECTIONS {\n"
-
-        script_contents += "\t/* Code sections */\n"
-        for address in code_sections:
-            # Same section name as in the ASM wrapper
-            address_str = hex(address)
-            section_name = ".text.%s" % address_str
-            script_contents += "\t%s %s : { *(%s) }\n" % (section_name,
-                                                          address_str,
-                                                          section_name)
-
-        script_contents += "\t/* Data sections */\n"
-        for variable in test_definition.variables:
-            # Same section name as in the ASM wrapper
-            address_str = hex(variable.address)
-            section_name = ".data.%s" % variable.name
-            script_contents += "\t%s %s : { *(%s) }\n" % (section_name,
-                                                          address_str,
-                                                          section_name)
-
-        script_contents += "}"
-
-        script_file = open(ldscriptfile, "w")
-        script_file.write(script_contents)
-        script_file.close()
-
     return
 
 
@@ -396,48 +266,13 @@ def main():
     )
 
     cmdline.add_option(
-        "output-file",
+        "c-output-file",
         "O",
         None,
-        "Output file name",
+        "C output file name",
         group=groupname,
-        opt_type=new_file_ext([".s", ".c"]),
+        opt_type=new_file_ext(".c"),
         required=True
-    )
-
-    cmdline.add_option(
-        "output-ld-script",
-        "L",
-        None,
-        "Output linker script file name",
-        group=groupname
-    )
-
-    cmdline.add_flag(
-        "safe-bin", None, "Ignore unrecognized binary codifications (do not"
-                          "fail). Useful when MPTs are generated by dumping "
-                          "directly code "
-                          "pages, which contain padding zeros and other "
-                          "non-code stuff)",
-        groupname,
-    )
-    cmdline.add_flag(
-        "raw-bin", None, "Process all instruction entries together. They "
-                         "all shoud be binary entries. Implies --safe-bin "
-                         "flag. Useful when MPTs are generated by dumping "
-                         "directly code "
-                         "pages, which contain padding zeros and other "
-                         "non-code stuff)",
-        groupname,
-    )
-    cmdline.add_flag(
-        "endless", None, "Wrap the code generated within an endless loop",
-        groupname
-    )
-    cmdline.add_flag(
-        "wrap-function", None, "Wrap the code generated as a function "
-                               "instead of a regular branch",
-        groupname
     )
 
     groupname = "Fixing options"
@@ -462,12 +297,25 @@ def main():
         " storage address computations (implies "
         "--fix-memory-referenes flag)", groupname
     )
-
-    groupname = "Memory state options"
-    cmdline.add_group(groupname, "Command arguments related to the memory "
-                                 "state")
     cmdline.add_flag(
-        "reset-registers", None, "Reset registers after each iteration",
+        "safe-bin", None, "Ignore unrecognized binary codifications (do not"
+                          "fail). Useful when MPTs are generated by dumping "
+                          "directly code "
+                          "pages, which contain padding zeros and other "
+                          "non-code stuff)",
+        groupname,
+    )
+    cmdline.add_flag(
+        "raw-bin", None, "Process all instruction entries together. They "
+                         "all shoud be binary entries. Implies --safe-bin "
+                         "flag. Useful when MPTs are generated by dumping "
+                         "directly code "
+                         "pages, which contain padding zeros and other "
+                         "non-code stuff)",
+        groupname,
+    )
+    cmdline.add_flag(
+        "endless", None, "Wrap the code generated within an endless loop",
         groupname
     )
 
@@ -493,15 +341,10 @@ def _main(arguments):
     print_info("Importing target definition...")
     target = import_definition(arguments.pop('target'))
     test_definition = arguments.pop('mpt_definition')
-    outputfile = arguments.pop('output_file')
-
-    if "output_ld_script" in arguments:
-        ldscriptfile = arguments.pop('output_ld_script')
-    else:
-        ldscriptfile = outputfile + ".lds"
+    outputfile = arguments.pop('c_output_file')
 
     print_info("Start generating '%s'" % outputfile)
-    generate(test_definition, outputfile, ldscriptfile, target, **arguments)
+    generate(test_definition, outputfile, target, **arguments)
     print_info("'%s' generated!" % outputfile)
 
 
